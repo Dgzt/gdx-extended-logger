c4bdd1b24a061f68a5500537ca343d8e557f08d9
Zsuró Tibor <zsurotibor@gmail.com>
2026-03-26 14:42:28 +0100

    StringBuilder Pool instead of single static variable

--- api/src/main/java/com/github/dgzt/extendedlogger/LoggerUtil.java
+++ api/src/main/java/com/github/dgzt/extendedlogger/LoggerUtil.java
@@ -1,21 +1,28 @@
 package com.github.dgzt.extendedlogger;
 
+import com.badlogic.gdx.utils.DefaultPool;
+import com.badlogic.gdx.utils.Pool;
+
 public class LoggerUtil {
 
+    private static final Pool<StringBuilder> SB_POOL = new DefaultPool<>(StringBuilder::new);
+
     private static final String MARKER = "{}";
-    private static final StringBuilder STRING_BUILDER = new StringBuilder();
 
     public static String format(final String message, String... arguments) {
         int i = 0;
-        STRING_BUILDER.append(message);
+        final StringBuilder sb = SB_POOL.obtain();
+        sb.setLength(0);
+        sb.append(message);
 
-        while (i < arguments.length && STRING_BUILDER.indexOf(MARKER) >= 0) {
-            final int index = STRING_BUILDER.indexOf(MARKER);
-            STRING_BUILDER.replace(index, index + MARKER.length(), arguments[i++]);
+        while (i < arguments.length && sb.indexOf(MARKER) >= 0) {
+            final int index = sb.indexOf(MARKER);
+            sb.replace(index, index + MARKER.length(), arguments[i++]);
         }
 
-        final String result = STRING_BUILDER.toString();
-        STRING_BUILDER.setLength(0);
+        final String result = sb.toString();
+        sb.setLength(0);
+        SB_POOL.free(sb);
         return result;
     }
 }
